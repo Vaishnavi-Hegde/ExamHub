b68e85648d1f74e07c9dbc046cf34b3a5f79d03b
vaibhavhegde15 <vaibhavhegde15@gmail.com>
2024-06-02 13:11:59 +0530

    changes to db name

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect('mongodb://127.0.0.1:27017/userSchema');
+mongoose.connect('mongodb://127.0.0.1:27017/examhub');
 
 app.use('/api/admin', adminRoutes);
 app.use('/api/student', studentRoutes);
